f10aefc227141be5fa9b204403d8268be3e9f127
Davidnazal <davidnazal123@gmail.com>
2025-08-26 09:01:51 +0700

    Week 1

---
+++ David_project1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.david_project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class David_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: David Nazal Farihin");
+        System.out.println("NPM : 2410506005");
+        System.out.println("Alamat : Wonosobo");
+        System.out.println("No_HP : 088212345626");
+        
+    }
+}
