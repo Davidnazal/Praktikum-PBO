d4b720ec64a2c7f8f0f02d8454701cac17161f0f
Davidnazal <davidnazal123@gmail.com>
2025-11-13 18:32:34 +0700

    week 10

---
+++ PraktikumPBO_10/src/main/java/Praktikum10/Tugas/MainPembayaran.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Praktikum10.Tugas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MainPembayaran {
+    public static void main(String[] args) {
+
+        Pembayaran elektronik = new Elektronik();
+        Pembayaran makanan = new Makanan();
+
+        double hargaElektronik = 2000000;
+        double hargaMakanan = 50000;
+
+        System.out.println("Sistem Pembayaran Toko");
+        System.out.println("Harga Elektronik : Rp " + hargaElektronik);
+        System.out.println("Pajak Elektronik (10%) : Rp " + elektronik.hitungPajak(hargaElektronik));
+        System.out.println("Harga Makanan : Rp " + hargaMakanan);
+        System.out.println("Pajak Makanan (5%) : Rp " + makanan.hitungPajak(hargaMakanan));
+    }
+}
